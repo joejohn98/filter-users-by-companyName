a25a031beeec69e90bd8a33a31006c3db60e14da
joejohn98 <joejohn048@gmail.com>
2025-07-01 21:06:12 +0530

    Add state management for users and loading/error handling in CompanyFilter component

--- src/CompanyFilter.tsx
+++ src/CompanyFilter.tsx
@@ -1,5 +1,11 @@
+import { useState } from "react"
+import type { User } from "./data/dummyFetch"
 
 const CompanyFilter = () => {
+    const [users, setUsers] = useState<User[]>([]);
+    const [selectedCompany, setSelectedCompany] = useState<string>("")
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>("")
 
   return (
     <div>
