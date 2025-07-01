f1f37ba95362e3a70dfbdc6f0a92297fd849dc24
joejohn98 <joejohn048@gmail.com>
2025-07-01 21:11:06 +0530

    Add user fetching logic and loading/error handling in CompanyFilter component

--- src/CompanyFilter.tsx
+++ src/CompanyFilter.tsx
@@ -1,11 +1,32 @@
-import { useState } from "react"
-import type { User } from "./data/dummyFetch"
+import { useEffect, useState } from "react"
+import { dummyFetch, type User } from "./data/dummyFetch"
 
 const CompanyFilter = () => {
     const [users, setUsers] = useState<User[]>([]);
     const [selectedCompany, setSelectedCompany] = useState<string>("")
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [error, setError] = useState<string>("")
+
+    useEffect(()=>{
+        const fetchUsers = async () => {
+            setIsLoading(true)
+            try {
+                const response = await dummyFetch('https://example.com/api/users')
+                if(response.status === 200){
+                    setUsers(response.data)
+                }
+            } catch (error ) {
+                if (error instanceof Error) {
+                    setError(error.message)
+                } else {
+                    setError("An unexpected error occurred.")
+                }
+            } finally{
+                setIsLoading(false)
+            }
+        }
+        fetchUsers()
+    }, [])
 
   return (
     <div>
